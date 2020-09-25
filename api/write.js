7bf8bdcd3f7d0999c347d5326ad9340995b14bfd
cspsBLord <benoit.lord@canada.ca>
2020-09-25 09:37:51 -0400

    Trying something else

--- api/write.js
+++ api/write.js
@@ -2,19 +2,30 @@ var fs = require('fs');
 var express = require("express");
 var app = express();
 
+var msg = "sessions.json updated";
+
 app.use(function(req, res, next){
   var content = req.query.content || '{ "participant1": { "questionAt": 3, "score": 7 }, "participant2": { "questionAt": 13, "score": -6 }, "participant3": { "questionAt": 13, "score": 0 } }';
 
-  fs.writeFile('sessions.json', content, function (err) {
+  /*fs.writeFileSync('static/sessions.json', content, function (err) {
     if (err) {
-      return console.log(err);
+      msg = "There was an error";
+      console.log(err);
     }
     else{
+      msg = "It worked";
       console.log('sessions.json updated');
     }
-  });
+  });*/
 
-  res.send("Test");
+  try {
+    fs.writeFileSync('sessions.json', content);
+  } catch(err) {
+    msg = error;
+    console.error(err);
+  }
+
+  res.send(msg);
 
   //next();
 });
