ee985e3df19e9f94a569ee2305dab9fae5f294ab
cspsBLord <benoit.lord@canada.ca>
2020-09-25 09:57:03 -0400

    Trying something else

--- api/write.js
+++ api/write.js
@@ -7,19 +7,8 @@ var msg = "sessions.json updated";
 app.use(function(req, res, next){
   var content = req.query.content || '{ "participant1": { "questionAt": 3, "score": 7 }, "participant2": { "questionAt": 13, "score": -6 }, "participant3": { "questionAt": 13, "score": 0 } }';
 
-  /*fs.writeFileSync('static/sessions.json', content, function (err) {
-    if (err) {
-      msg = "There was an error";
-      console.log(err);
-    }
-    else{
-      msg = "It worked";
-      console.log('sessions.json updated');
-    }
-  });*/
-
   try {
-    fs.writeFileSync('../sessions.json', content);
+    fs.writeFileSync('../../sessions.json', content);
   } catch(err) {
     msg = error;
     console.error(err);
