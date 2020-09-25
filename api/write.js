d98052fa03878fd2ba62b91aca29a91686a51749
cspsBLord <benoit.lord@canada.ca>
2020-09-25 10:57:02 -0400

    Fixes in api for localhost

--- api/write.js
+++ api/write.js
@@ -3,20 +3,39 @@ var express = require("express");
 var app = express();
 
 var msg = "sessions.json updated";
+const isServerlessEnvironment = process.env.ON_VERCEL=="true";
 
-app.use(function(req, res, next){
-  var content = req.query.content || '{ "participant1": { "questionAt": 3, "score": 7 }, "participant2": { "questionAt": 13, "score": -6 }, "participant3": { "questionAt": 13, "score": 0 } }';
+if(isServerlessEnvironment){
+  app.use(function(req, res, next){
+    var content = req.query.content || '{ "participant1": { "questionAt": 3, "score": 7 }, "participant2": { "questionAt": 13, "score": -6 }, "participant3": { "questionAt": 13, "score": 0 } }';
 
-  try {
-    fs.writeFileSync('/tmp/sessions.json', content);
-  } catch(err) {
-    msg = err;
-    console.error(err);
-  }
+    try {
+      fs.writeFileSync("/tmp/sessions.json", content);
+    } catch(err) {
+      msg = err;
+      console.error(err);
+    }
 
-  res.send(msg);
+    res.send(msg);
 
-  //next();
-});
+    next();
+  });
+}
+else{
+  app.use("/en/api/write", function(req, res, next){
+    var content = req.query.content || '{ "participant1": { "questionAt": 3, "score": 7 }, "participant2": { "questionAt": 13, "score": -6 }, "participant3": { "questionAt": 13, "score": 0 } }';
+
+    try {
+      fs.writeFileSync("static/sessions.json", content);
+    } catch(err) {
+      msg = err;
+      console.error(err);
+    }
+
+    res.send(msg);
+
+    next();
+  });
+}
 
 module.exports = app
